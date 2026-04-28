a01757088ec22d4d2b1c545bcfbc2e067afc4b08
abstractlabs-dev <info.abstractlabs@gmail.com>
2026-04-28 22:31:30 +0530

    fix: component styles for light mode and force default light theme

--- app/providers.jsx
+++ app/providers.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }) {
   return (
-    <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem={false}>
+    <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem={false} storageKey="abstract-theme-v1">
       {children}
     </ThemeProvider>
   );
